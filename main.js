05b6b6b90a42dc8c5a90cd19b4dd2d60324a5f9f
Thomas <thomascaycedo19@outlook.com>
2018-12-27 21:48:25 -0500

    Se agregan libros

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
 //Clase libro
-class Libro{
-    constructor(titulo,autor,id){
+class Libro {
+    constructor(titulo, autor, id) {
         this.titulo = titulo;
-        this.autor  = autor;
+        this.autor = autor;
         this.id = id;
     }
 }
 
 //Manejador de la clase
-class UI{
-    static desplegarLibro(){
-        const LibrosGuardados = [
+class UI {
+    static desplegarLibro() {
+        const LibrosGuardados = [{
+                titulo: 'Libro Uno',
+                autor: 'Thomas1',
+                id: '123'
+            },
             {
-            titulo: 'Libro Uno',
-            autor: 'Thomas1',
-            id: '123'
-        },
-        {
-            titulo: 'Libro Dos',
-            autor: 'Thomas2',
-            id: '321'
-        }
-    ];
-    const libros = LibrosGuardados;
+                titulo: 'Libro Dos',
+                autor: 'Thomas2',
+                id: '321'
+            }
+        ];
+        const libros = LibrosGuardados;
 
-    libros.forEach((libro)=> UI.agregarLibroLista(libro));
+        libros.forEach((libro) => UI.agregarLibroLista(libro));
     }
 
-    static agregarLibroLista(libro){
+    static agregarLibroLista(libro) {
 
         const lista = document.querySelector('#lista-libro')
 
@@ -46,4 +45,24 @@ class UI{
 
 
 //Desplegar libro en el DOM 
-document.addEventListener('DOMContentLoaded',UI.desplegarLibro);
+document.addEventListener('DOMContentLoaded', UI.desplegarLibro);
+
+//Evento Agregar libro
+
+document.querySelector('#libro-form').addEventListener('submit', (e) => {
+    // prevent submit
+    e.preventDefault();
+
+    //Obtener datos del formulario
+    const titulo = document.querySelector('#titulo').value;
+    const autor = document.querySelector('#autor').value;
+    const id = document.querySelector('#id').value;
+
+    //Instanciar libro
+    const libro = new Libro(titulo, autor, id);
+
+    //console.log(libro)
+
+    //Añadir el libro a la lista
+    UI.agregarLibroLista(libro);
+});
