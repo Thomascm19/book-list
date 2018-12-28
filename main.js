227408ca160706875c6612ab4daae545f88c3c6f
Thomas <thomascaycedo19@outlook.com>
2018-12-27 21:34:45 -0500

    Se muestran los libros por defecto

---
+++ main.js
@@ -0,0 +1,49 @@
+//Clase libro
+class Libro{
+    constructor(titulo,autor,id){
+        this.titulo = titulo;
+        this.autor  = autor;
+        this.id = id;
+    }
+}
+
+//Manejador de la clase
+class UI{
+    static desplegarLibro(){
+        const LibrosGuardados = [
+            {
+            titulo: 'Libro Uno',
+            autor: 'Thomas1',
+            id: '123'
+        },
+        {
+            titulo: 'Libro Dos',
+            autor: 'Thomas2',
+            id: '321'
+        }
+    ];
+    const libros = LibrosGuardados;
+
+    libros.forEach((libro)=> UI.agregarLibroLista(libro));
+    }
+
+    static agregarLibroLista(libro){
+
+        const lista = document.querySelector('#lista-libro')
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${libro.titulo}</td>
+            <td>${libro.autor}</td>
+            <td>${libro.id}</td>
+            <td><a href="#" class="btn btn-danger">X</a></td>
+        `;
+
+        lista.appendChild(row);
+    }
+}
+
+
+//Desplegar libro en el DOM 
+document.addEventListener('DOMContentLoaded',UI.desplegarLibro);
