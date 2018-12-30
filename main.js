1ce4edf18ca490431c02706f33352d367a1b1a0a
Thomas <thomascaycedo19@outlook.com>
2018-12-29 20:23:38 -0500

    Se crea la alerta de validacion, de remueve en 3 segundos

--- main.js
+++ main.js
@@ -36,18 +36,41 @@ class UI {
             <td>${libro.titulo}</td>
             <td>${libro.autor}</td>
             <td>${libro.id}</td>
-            <td><a href="#" class="btn btn-danger">X</a></td>
-        `;
+            <td><a href="#" class="btn btn-danger btn-sm borrar">X</a></td>`;
 
         lista.appendChild(row);
 
     }
+    //Metodo borrar la fila del libro.
+    static borrarCampo(borr) {
+        if (borr.classList.contains('borrar')) {
+            borr.parentElement.parentElement.remove()
+        }
+    }
 
-    static limpliarCampos(){
+    //Alerta de validacion 
+    static mostrarAlerta(mensaje, className){
+
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(mensaje));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#libro-form');
+        //Insertar el div antes de la etiqueta form
+        container.insertBefore(div, form);
+        //Desaparecer en 3 segundos
+        setTimeout(()=> document.querySelector('.alert').remove(),3000);
+        
+    }
+
+    //Metodo limpiar campos del form
+    static limpliarCampos() {
         document.querySelector('#titulo').value = '';
         document.querySelector('#autor').value = '';
         document.querySelector('#id').value = '';
     }
+
+
 }
 
 
@@ -60,20 +83,34 @@ document.querySelector('#libro-form').addEventListener('submit', (e) => {
     // prevent submit
     e.preventDefault();
 
+
     //Obtener datos del formulario
     const titulo = document.querySelector('#titulo').value;
     const autor = document.querySelector('#autor').value;
     const id = document.querySelector('#id').value;
 
-    //Instanciar libro
-    const libro = new Libro(titulo, autor, id);
+    //Validacion del form
+    if (titulo === '' || autor === '' || id === '') {
 
-    //console.log(libro)
+       UI.mostrarAlerta('Complete los campos','danger');
+       
+    } else {
 
-    //Añadir el libro a la lista
-    UI.agregarLibroLista(libro);
+        //Instanciar libro
+        const libro = new Libro(titulo, autor, id);
 
-    //Limpiar campos
+        //console.log(libro)
 
-    UI.limpliarCampos();
+        //Añadir el libro a la lista
+        UI.agregarLibroLista(libro);
+
+        //Limpiar campos
+
+        UI.limpliarCampos();
+    }
 });
+
+
+//document.querySelector('#lista-libro').addEventListener('click', (e) => {
+ //   console.log(e.target);
+//});
